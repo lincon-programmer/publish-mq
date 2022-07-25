3c62f1381a95f6fbefc2f76567f7ba299f347ce5
webtechcompany <lincon.santos@contact-webtechcompany.com.br>
2022-07-25 15:25:01 -0300

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { test_mq } from './mq-conect/mqlight.connect';
+import { test_mq } from './mq-conect/brocker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- src/mq-conect/brocker.ts
+++ src/mq-conect/brocker.ts
@@ -13,6 +13,27 @@ function sleep(ms: number) {
   });
 }
 
+function publishMessage(hObj: mq.MQObject) {
+  const msg = 'Hello from Node at ' + new Date().toString();
+
+  const mqmd = new mq.MQMD(); // Defaults are fine.
+  const pmo = new mq.MQPMO();
+
+  // Describe how the Publish (Put) should behave
+  pmo.Options =
+    MQC.MQPMO_NO_SYNCPOINT | MQC.MQPMO_NEW_MSG_ID | MQC.MQPMO_NEW_CORREL_ID;
+  // Add in the flag that gives a warning if noone is
+  // subscribed to this topic.
+  pmo.Options |= MQC.MQPMO_WARN_IF_NO_SUBS_MATCHED;
+  mq.Put(hObj, mqmd, pmo, msg, function (err) {
+    if (err) {
+      console.error(formatErr(err));
+    } else {
+      console.log('MQPUT successful');
+    }
+  });
+}
+
 // The program starts here.
 // Connect to the queue manager.
 console.log('Sample AMQSCONN.TS start');
@@ -22,8 +43,8 @@ const cno = <any>new mq.MQCNO();
 
 // Add authentication via the MQCSP structure
 const csp = new mq.MQCSP();
-csp.UserId = 'lincongallo';
-csp.Password = 'vehs0EFW1-LnwkJOvKI8c5LdbNx0QHjZq87FXUfEwGWW';
+csp.UserId = '';
+csp.Password = '';
 // Make the MQCNO refer to the MQCSP
 // This line allows use of the userid/password
 cno.SecurityParms = csp;
@@ -33,8 +54,8 @@ cno.SecurityParms = csp;
 cno.Options |= MQC.MQCNO_CLIENT_BINDING;
 // And then fill in relevant fields for the MQCD
 const cd = new mq.MQCD();
-cd.ConnectionName = 'saturno-5c7d.qm.us-south.mq.appdomain.cloud(32473)';
-cd.ChannelName = 'CLOUD.ADMIN.SVRCONN';
+cd.ConnectionName = '';
+cd.ChannelName = '';
 // Make the MQCNO refer to the MQCD
 cno.ClientConn = cd;
 
